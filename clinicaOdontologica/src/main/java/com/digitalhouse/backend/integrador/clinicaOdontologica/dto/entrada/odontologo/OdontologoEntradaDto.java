6e1e6802dc33a61d439f4480038b8e59a6395107
juannmachado1995 <juannmachado1995@gmail.com>
2023-09-03 20:40:19 -0300

    odonto

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/dto/entrada/odontologo/OdontologoEntradaDto.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/dto/entrada/odontologo/OdontologoEntradaDto.java
@@ -1,4 +1,63 @@
 package com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.odontologo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.apache.logging.log4j.message.Message;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OdontologoEntradaDto {
+    @NotNull(message = "Numero Matricula no puede ser nulo")
+    @NotBlank(message = "Debe tener numero matricula")
+    @Pattern(regexp = "^[A-Z]{2}-\\d{1,3}\\d*$")
+    @Size(min = 10, message = "La matricula debe tener 10 caracteres")
+    @Size(max = 10,message = "La matricula debe tener 10 caracteres")
+    private int numeroMatricula;
+
+    @NotBlank(message = "Odontolo debe tener nombre")
+    @NotNull(message = "Nombre de odontologo no puede ser nulo")
+    @Size(max = 50,message = "El nombre odontologo debe tener maximo 50 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "Odontolo debe tener Apellido")
+    @NotNull(message = "Apellido de odontologo no puede ser nulo")
+    @Size(max = 50,message = "El apellido odontologo debe tener maximo 50 caracteres")
+    private String apellido;
+
+    public OdontologoEntradaDto(int numeroMatricula, String nombre, String apellido) {
+
+        this.numeroMatricula = numeroMatricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public OdontologoEntradaDto() {
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
