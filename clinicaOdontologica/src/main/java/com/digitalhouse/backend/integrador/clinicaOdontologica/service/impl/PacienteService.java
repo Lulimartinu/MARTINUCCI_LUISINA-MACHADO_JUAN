b2ad6eb7db243a707513f720f600899ae084fa69
juannmachado1995 <juannmachado1995@gmail.com>
2023-09-05 00:11:00 -0300

    sadafgag

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/PacienteService.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/PacienteService.java
@@ -1,4 +1,37 @@
 package com.digitalhouse.backend.integrador.clinicaOdontologica.service.impl;
 
-public class PacienteService  {
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.modificacion.PacienteModificacionEntrada;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.paciente.PacienteEntradaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.salida.paciente.PacienteSalidaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.service.IPacienteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PacienteService  implements IPacienteService {
+    @Override
+    public List<PacienteSalidaDto> detallarPacientes() {
+        return null;
+    }
+
+    @Override
+    public PacienteSalidaDto crearPaciente(PacienteEntradaDto paciente) {
+        return null;
+    }
+
+    @Override
+    public void eliminarPacientePorId(Long id) {
+
+    }
+
+    @Override
+    public PacienteSalidaDto buscarPacientePorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public PacienteSalidaDto actualizarPaciente(PacienteModificacionEntrada pacienteModificacionEntrada) {
+        return null;
+    }
 }
--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/TurnoService.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/TurnoService.java
@@ -1,4 +1,37 @@
 package com.digitalhouse.backend.integrador.clinicaOdontologica.service.impl;
 
-public class TurnoService {
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.modificacion.TurnoModificacionEntrada;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.turno.TurnoEntradaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.salida.turno.TurnoSalidaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.service.ITurnoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TurnoService implements ITurnoService {
+    @Override
+    public List<TurnoSalidaDto> detallarTurnos() {
+        return null;
+    }
+
+    @Override
+    public TurnoSalidaDto crearTurno(TurnoEntradaDto turno) {
+        return null;
+    }
+
+    @Override
+    public void eliminarTurnoPorId(Long id) {
+
+    }
+
+    @Override
+    public TurnoSalidaDto buscarTurnoPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public TurnoSalidaDto actualizarTurno(TurnoModificacionEntrada turnoModificacionEntrada) {
+        return null;
+    }
 }
