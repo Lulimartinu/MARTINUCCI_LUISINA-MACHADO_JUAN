ab1d797ddb0b05e17d98f45e47b15698e6782acf
Lulimartinu <lulimartinucci@hotmail.com>
2023-09-06 00:11:03 -0300

    asas

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/OdontologoService.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/OdontologoService.java
@@ -29,25 +29,34 @@ public class OdontologoService implements IOdontologoService {
 
     @Override
     public List<OdontologoSalidaDto> detallarOdontologos() {
-        List<OdontologoSalidaDto> odontologos = odontoRepository.findAll().stream().map(o -> modelMapper.map(o, OdontologoSalidaDto.class)).toList();
+        List<OdontologoSalidaDto> odontologos = odontoRepository.findAll().stream().map(this::entidadADtoSalida).toList();
         LOGGER.info("lista de todos los odontologos disponibles :{}",odontologos);
         return odontologos;
     }
 
     @Override
     public OdontologoSalidaDto crearOdontologo(OdontologoEntradaDto odontologo) {
-        Odontologo odontoGuardado = odontoRepository.save(modelMapper.map(odontologo, Odontologo.class));
-        OdontologoSalidaDto odontologoSalidaDto = modelMapper.map(odontoGuardado, OdontologoSalidaDto.class);
+        Odontologo odontoGuardado = odontoRepository.save(dtoEntradaAEntidad(odontologo));
+        OdontologoSalidaDto odontologoSalidaDto = entidadADtoSalida(odontoGuardado);
         LOGGER.info("Se ha creado un nuevo Odontologo : {}", odontologoSalidaDto);
         return odontologoSalidaDto;
     }
 
     @Override
     public OdontologoSalidaDto buscarOdontologoPorId(Long id) {
+        Odontologo odontoBuscado = odontoRepository.findById(id).orElse(null);
 
-
-        return null;
+        OdontologoSalidaDto odontologoSalidaDto = null;
+        if(odontologoSalidaDto != null){
+            odontologoSalidaDto = entidadADtoSalida(odontoBuscado);
+            LOGGER.info("Se ha encontrado el Odontologo: {}", odontologoSalidaDto);
+        }
+        else{
+            LOGGER.error("No se ha encontrado un Odontologo en la BDD con ese id");
+        }
+        return odontologoSalidaDto;
     }
+
 
     @Override
     public void eliminarOdontologoPorId(Long id) {
@@ -58,4 +67,18 @@ public class OdontologoService implements IOdontologoService {
     public OdontologoSalidaDto actualizarOdontologo(OdontologoModificacionEntrada odontologoModificacionEntrada) {
         return null;
     }
+
+    public Odontologo dtoEntradaAEntidad(OdontologoEntradaDto odontologoEntradaDto){
+        return modelMapper.map(odontologoEntradaDto, Odontologo.class);
+    }
+
+    public OdontologoSalidaDto entidadADtoSalida(Odontologo odontologo){
+        return modelMapper.map(odontologo,OdontologoSalidaDto.class);
+    }
+
+    public Odontologo dtoModificacionAEntidad(OdontologoModificacionEntrada odontologoModificacionEntrada){
+        return modelMapper.map(odontologoModificacionEntrada, Odontologo.class);
+    }
+
+
 }
