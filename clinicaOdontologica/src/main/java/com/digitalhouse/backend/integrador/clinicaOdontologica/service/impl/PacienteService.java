de631fcfcf4de33633f0b14cd5f49bf2310a826f
Lulimartinu <lulimartinucci@hotmail.com>
2023-09-08 19:00:00 -0300

    hago cambios pacienteService

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/PacienteService.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/PacienteService.java
@@ -3,13 +3,35 @@ package com.digitalhouse.backend.integrador.clinicaOdontologica.service.impl;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.modificacion.PacienteModificacionEntrada;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.paciente.PacienteEntradaDto;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.salida.paciente.PacienteSalidaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.repository.OdontoRepository;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.repository.PacienteRepository;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.service.IPacienteService;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PacienteService  implements IPacienteService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(PacienteService.class);
+
+
+    private final PacienteRepository pacienteRepository;
+
+    private final ModelMapper modelMapper;
+
+
+    public PacienteService() {
+    }
+
+    public PacienteService(PacienteRepository pacienteRepository, ModelMapper modelMapper) {
+        this.pacienteRepository = pacienteRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public List<PacienteSalidaDto> detallarPacientes() {
         return null;
