eaf108fe1d244f789870ce910a1a3d92757f9520
Lulimartinu <lulimartinucci@hotmail.com>
2023-09-08 19:30:15 -0300

    empezamos turnoService

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/TurnoService.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/service/impl/TurnoService.java
@@ -2,14 +2,33 @@ package com.digitalhouse.backend.integrador.clinicaOdontologica.service.impl;
 
 import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.modificacion.TurnoModificacionEntrada;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.turno.TurnoEntradaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.salida.turno.OdontoTurnoSalidaDto;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.salida.turno.TurnoSalidaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.repository.TurnoRepository;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.service.ITurnoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TurnoService implements ITurnoService {
+
+    private final TurnoRepository turnoRepository;
+    private final ModelMapper modelMapper;
+    private final OdontologoService odontologoService;
+    private final PacienteService pacienteService;
+
+    public TurnoService() {
+    }
+
+    public TurnoService(TurnoRepository turnoRepository, ModelMapper modelMapper, OdontologoService odontologoService, PacienteService pacienteService) {
+        this.turnoRepository = turnoRepository;
+        this.modelMapper = modelMapper;
+        this.odontologoService = odontologoService;
+        this.pacienteService = pacienteService;
+    }
+
     @Override
     public List<TurnoSalidaDto> detallarTurnos() {
         return null;
@@ -34,4 +53,24 @@ public class TurnoService implements ITurnoService {
     public TurnoSalidaDto actualizarTurno(TurnoModificacionEntrada turnoModificacionEntrada) {
         return null;
     }
+
+    private PacienteTurnoSalidaDto pacSalDtoASalTurnoDto(Long id) {
+        return modelMapper.map(pacienteService.buscarPacientePorId(id), PacienteTurnoSalidaDto.class);
+    }
+
+    private OdontoTurnoSalidaDto odontoSalDtoASalTurnoDto(Long id) {
+        return modelMapper.map(odontologoService.buscarOdontologoPorId(id), OdontoTurnoSalidaDto.class);
+    }
+
+    private TurnoSalidaDto entidadADto(Turno turno) {
+        TurnoSalidaDto turnoSalidaDto = modelMapper.map(turno, TurnoSalidaDto.class);
+        turnoSalidaDto.setPacienteTurnoSalidaDto(pacienteSalidaDtoASalidaTurnoDto(turno.getPaciente().getId()));
+        turnoSalidaDto.setOdontologoTurnoSalidaDto(odontologoSalidaDtoASalidaTurnoDto(turno.getOdontologo().getId()));
+        return turnoSalidaDto;
+
+    }
+
+
+
+
 }
