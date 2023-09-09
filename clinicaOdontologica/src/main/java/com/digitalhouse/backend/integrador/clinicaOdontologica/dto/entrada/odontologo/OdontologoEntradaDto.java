2e81661304d643c3f20caa8ff496a0fb2c70da18
Lulimartinu <lulimartinucci@hotmail.com>
2023-09-09 16:10:11 -0300

    arreglo atributos odontologoEntradaDto

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/dto/entrada/odontologo/OdontologoEntradaDto.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/dto/entrada/odontologo/OdontologoEntradaDto.java
@@ -19,18 +19,18 @@ public class OdontologoEntradaDto {
     @NotBlank(message = "Odontologo debe tener nombre")
     @NotNull(message = "Nombre de odontologo no puede ser nulo")
     @Size(max = 50,message = "El nombre odontologo debe tener maximo 50 caracteres")
-    private String nombre;
+    private String nombreOdontologo;
 
     @NotBlank(message = "Odontologo debe tener Apellido")
     @NotNull(message = "Apellido de odontologo no puede ser nulo")
     @Size(max = 50,message = "El apellido odontologo debe tener maximo 50 caracteres")
-    private String apellido;
+    private String apellidoOdontologo;
 
-    public OdontologoEntradaDto(String numeroMatricula, String nombre, String apellido) {
+    public OdontologoEntradaDto(String numeroMatricula, String nombreOdontologo, String apellidoOdontologo) {
 
         this.numeroMatricula = numeroMatricula;
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this.nombreOdontologo = nombreOdontologo;
+        this.apellidoOdontologo = apellidoOdontologo;
     }
 
     public OdontologoEntradaDto() {
@@ -45,18 +45,18 @@ public class OdontologoEntradaDto {
     }
 
     public String getNombre() {
-        return nombre;
+        return nombreOdontologo;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre(String nombreOdontologo) {
+        this.nombreOdontologo = nombreOdontologo;
     }
 
     public String getApellido() {
-        return apellido;
+        return apellidoOdontologo;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public void setApellido(String apellidoOdontologo) {
+        this.apellidoOdontologo = apellidoOdontologo;
     }
 }
