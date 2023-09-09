a1e57bceb3cdf289abb79c63ca4716356007cac4
Lulimartinu <lulimartinucci@hotmail.com>
2023-09-09 10:02:20 -0300

    buscamos arreglar el error

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/ClinicaOdontologicaApplication.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -1,13 +1,20 @@
 package com.digitalhouse.backend.integrador.clinicaOdontologica;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
