22fc207c478c6e14830a586bace994979e8f33d7
Lulimartinu <lulimartinucci@hotmail.com>
2023-09-10 13:37:01 -0300

    agregamos logger a app

--- clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/ClinicaOdontologicaApplication.java
+++ clinicaOdontologica/src/main/java/com/digitalhouse/backend/integrador/clinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -1,20 +1,27 @@
 package com.digitalhouse.backend.integrador.clinicaOdontologica;
 
+import org.apache.juli.logging.Log;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
+
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClinicaOdontologicaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+		LOGGER.info("LA CLINICA ODONTOLÓGICA MM ARRANCÓ A FUNCIONAR");
 	}
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+
 
 }
