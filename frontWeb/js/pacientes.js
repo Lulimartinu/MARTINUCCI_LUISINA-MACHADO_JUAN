4b38667a840812c646997dcc178dbaafd90f648e
juannmachado1995 <juannmachado1995@gmail.com>
2023-09-13 19:07:13 -0300

    DetallarPacientes

--- frontWeb/js/pacientes.js
+++ frontWeb/js/pacientes.js
@@ -14,10 +14,49 @@ window.addEventListener('load', function () {
     let provincia = document.querySelector("#provinciaPaciente");
     let idPaciente = document.querySelector("#id-Paciente");
     let idPacienteEliminar = document.querySelector("#idPacienteEliminar");
- 
+
 
     let url = "http://localhost:8080";
 
+    detallarPacientes();
+
+
+    //DETALLAR PACIENTES
+    function detallarPacientes() {
+        const apiUrl = 'http://localhost:8080/pacientes/detallar';
+
+        // solicitud GET detallar
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('La solicitud no se completó con éxito');
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (!data || !Array.isArray(data)) {
+                    // Verificar si `data` es undefined o no es un arreglo
+                    throw new Error('Los datos no están en el formato esperado');
+                }
+
+                //Formatear datos
+                let formateoDatos = data.map(paciente => `<li>${paciente.id + " " + paciente.nombrePaciente + " " + paciente.apellidoPaciente}</li>`);
+
+                //elemento donde mostrar los datos
+                let listaPacientes = document.getElementById('detallarPacientes');
+
+                // Agregar el contenido HTML
+                listaPacientes.innerHTML = `<ul>${formateoDatos.join('')}</ul>`;
+            })
+            .catch(error => {
+                console.error('Ocurrió un error:', error);
+            });
+    }
+
+detallarPacientes();
+
+
+    //CREAR PACIENTES
     formularioCrear.addEventListener('submit', function (event) {
         event.preventDefault();
         let payload = {
@@ -42,7 +81,6 @@ window.addEventListener('load', function () {
                 "Content-Type": "application/json"
             }
         }
-
         crearPaciente(settings)
     })
 
@@ -50,16 +88,24 @@ window.addEventListener('load', function () {
         fetch(`${url}/pacientes/crear`, settings)
             .then(response => {
                 console.log(response);
-                if (response.ok) return response.json()
+                if (response.ok) return response.json();
 
-                console.log("algunos de los datos es incorrecto");
-                return Promise.reject(response)
+                console.log("Algunos de los datos son incorrectos");
+                return Promise.reject(response);
             })
-
+            .then(data => {
+                console.log(data);
+                // Después de crear un paciente, actualiza la lista de pacientes
+                detallarPacientes();
+            })
+            .catch(error => {
+                console.error('Ocurrió un error:', error);
+            });
     }
 
-    //BUSCAR PACIENTE X ID
 
+
+    //BUSCAR PACIENTE X ID
     formularioBuscar.addEventListener('submit', function (event) {
         event.preventDefault();
         // Definir la URL de la API
@@ -83,23 +129,26 @@ window.addEventListener('load', function () {
     })
 
 
-//ELIMINAR PACIENTE X ID
-formularioEliminar.addEventListener('submit', function (event) {
-    event.preventDefault();
-    
-    //ID del paciente
-    let idPac = idPacienteEliminar.value;
-    // url
-    let apiUrl = 'http://localhost:8080/pacientes/eliminar/' + idPac;
-    
-    // Configuración de la solicitud DELETE
-    let settings = {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json' 
-                }
-    };
-    
+
+
+
+    //ELIMINAR PACIENTE X ID
+    formularioEliminar.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        //ID del paciente
+        let idPac = idPacienteEliminar.value;
+        // url
+        let apiUrl = 'http://localhost:8080/pacientes/eliminar/' + idPac;
+
+        // Configuración de la solicitud DELETE
+        let settings = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
     // Realizar la solicitud DELETE
     fetch(apiUrl, settings)
         .then(response => {
@@ -110,11 +159,47 @@ formularioEliminar.addEventListener('submit', function (event) {
         })
         .then(data => {
             console.log(data);
+
+
+
+
+            //TODO ESTO ANDA PORQUE LO ELIMINA DE LA LISTA 
+            //EL PROBLEMA ES QUE NO ACTUALIZA LA LISTA !!! PERO ESTA TDO BIEN 
+            // ID del paciente q eliminamos
+            let idPac = idPacienteEliminar.value;
+            // aca va la Lista q contiene la lista de pacientes
+            let listaPacientes = document.getElementById('detallarPacientes');
+            // aca entramos a los  LI dentro dela UL
+            let elementosLi = listaPacientes.querySelectorAll('li');
+            // aca entramos a cada LI
+            elementosLi.forEach(elemento => {
+                //aca sacamos el texto que es "Id nombre apellido"
+                let textoElemento = elemento.textContent;
+                // si el texto tiene dentro el idPac que le pasamos lo eliminamos 
+                if (textoElemento.includes(idPac)) {
+                    // Elimina el pacient de  LI de la lista
+                    elemento.remove();
+                }
+            });
+
+            // ACA DEBERIA  actualizar la lista de pacientes pero no anda no se que onda 
+            //PARA MI ESTA LLAMADA A LA FUNCION VA EN OTRO LADO O NO SE 
+            detallarPacientes();
+
+
+
+
+
+
+
+
+
+
         })
         .catch(error => {
             console.error('Ocurrió un error:', error);
         });
 });
+detallarPacientes();
 
 });
-  
