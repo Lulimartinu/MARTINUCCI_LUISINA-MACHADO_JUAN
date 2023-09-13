1a132eab44d56e8f4d37e4bceaf2cb0da3987f29
juannmachado1995 <juannmachado1995@gmail.com>
2023-09-13 01:10:47 -0300

    Agregamos Metodo buscar PAciente

--- frontWeb/js/pacientes.js
+++ frontWeb/js/pacientes.js
@@ -1,86 +1,84 @@
 
-window.addEventListener('load',function(){
-    let formulario = document.querySelector('#form-paciente');
+window.addEventListener('load', function () {
+    let formularioCrear = document.querySelector('#form-paciente');
+    let formularioBuscar = document.querySelector('#paciente-buscar')
+    let formularioEliminar = this.document.querySelector('#paciente-eliminar')
+
     let nombrePaciente = document.querySelector("#nombrePaciente");
     let apellidoPaciente = document.querySelector("#apellidoPaciente");
     let dni = document.querySelector("#dniPaciente");
     let fechaIngreso = document.querySelector("#fechaIngresoPaciente");
     let calle = document.querySelector("#callePaciente");
-    let numeroCalle = document.querySelector("#numeroPaciente");
+    let numeroCalle = document.querySelector("#numeroCallePaciente");
     let localidad = document.querySelector("#localidadPaciente");
     let provincia = document.querySelector("#provinciaPaciente");
-    let idPaciente = document.querySelector("#idPaciente");
-    
+    let idPaciente = document.querySelector("#id-Paciente");
+ 
+
     let url = "http://localhost:8080";
 
-    formulario.addEventListener('submit',function(event){
+    formularioCrear.addEventListener('submit', function (event) {
         event.preventDefault();
         let payload = {
-            nombrePaciente : nombrePaciente.value,
-            apellidoPaciente : apellidoPaciente.value,
-            dni : dni.value,
-            fechaIngreso : fechaIngreso.value,
-            domicilio : {
-                calle : calle.value,
-                //numero : numeroCalle.value,
-                localidad : localidad.value,
-                provincia : provincia.value,
+            nombrePaciente: nombrePaciente.value,
+            apellidoPaciente: apellidoPaciente.value,
+            dni: dni.value,
+            fechaIngreso: fechaIngreso.value,
+            domicilio: {
+                calle: calle.value,
+                numero: numeroCalle.value,
+                localidad: localidad.value,
+                provincia: provincia.value,
             }
-            
+
         }
         console.log(payload)
 
         let settings = {
-            method : "POST",
-            body : JSON.stringify(payload),
+            method: "POST",
+            body: JSON.stringify(payload),
             headers: {
-                "Content-Type" : "application/json"
+                "Content-Type": "application/json"
             }
         }
-        
+
         crearPaciente(settings)
     })
 
-    function crearPaciente (settings){
+    function crearPaciente(settings) {
         fetch(`${url}/pacientes/crear`, settings)
-        .then(response => {
-            console.log(response);
-            if (response.ok) return response.json()
+            .then(response => {
+                console.log(response);
+                if (response.ok) return response.json()
 
-            console.log("algunos de los datos es incorrecto");
-            return Promise.reject(response)
-        })
-        
+                console.log("algunos de los datos es incorrecto");
+                return Promise.reject(response)
+            })
+
     }
 
+    //BUSCAR PACIENTE X ID
 
-
-//BUSCAR PACIENTE X ID
-    formulario.addEventListener('submit',function(event){
+    formularioBuscar.addEventListener('submit', function (event) {
         event.preventDefault();
-   
-       buscarPaciente()
+        // Definir la URL de la API
+        let idPac = idPaciente.value;
+        const apiUrl = 'http://localhost:8080/pacientes/';
+        // Realizar la solicitud GET
+        fetch(apiUrl + idPac)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('La solicitud no se completó con éxito');
+                }
+                return response.json(); // Parsear la respuesta JSON
+            })
+            .then(data => {
+                // Hacer algo con los datos recibidos (data)
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Ocurrió un error:', error);
+            });
     })
-    function buscarPaciente() {
-        const settings = {
-          method: "GET",
-          headers: {
-            
-          }
-        }
-        fetch(`${url}/pacientes/${idPaciente.value}`,settings)
-          .then(tareas => {
-            console.log("Tareas del usuario");
-            console.log(tareas);
-    
-            renderizarTareas(tareas)
-            botonesCambioEstado()
-            botonBorrarTarea()
-    
-          })
-          .catch(error => {
-            console.log(error);
-          })
-      };
 
 })
