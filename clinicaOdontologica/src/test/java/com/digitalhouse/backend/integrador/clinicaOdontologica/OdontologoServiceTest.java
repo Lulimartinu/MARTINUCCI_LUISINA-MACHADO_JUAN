faee6052c633d895dbb85d6deec5e0575169d941
Lulimartinu <lulimartinucci@hotmail.com>
2023-09-14 13:25:19 -0300

    crear un odonto con nombre luciana

--- clinicaOdontologica/src/test/java/com/digitalhouse/backend/integrador/clinicaOdontologica/OdontologoServiceTest.java
+++ clinicaOdontologica/src/test/java/com/digitalhouse/backend/integrador/clinicaOdontologica/OdontologoServiceTest.java
@@ -1,6 +1,7 @@
 package com.digitalhouse.backend.integrador.clinicaOdontologica;
 
 import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.entrada.odontologo.OdontologoEntradaDto;
+import com.digitalhouse.backend.integrador.clinicaOdontologica.dto.salida.odontologo.OdontologoSalidaDto;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.service.impl.OdontologoService;
 import com.digitalhouse.backend.integrador.clinicaOdontologica.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.Order;
@@ -48,4 +49,17 @@ public class OdontologoServiceTest {
         assertThrows(ResourceNotFoundException.class, () -> odontologoService.eliminarOdontologoPorId(2L));
 
     }
+
+   @Test
+    @Order(3)
+    void deberiaCrearUnOdontologoConNombreLuciana(){
+        OdontologoEntradaDto odontologoEntradaDto = new OdontologoEntradaDto("AA-123456789","Luciana","Murga");
+
+        OdontologoSalidaDto odontologoSalidaDto = odontologoService.crearOdontologo(odontologoEntradaDto);
+
+        assertEquals("Luciana",odontologoSalidaDto.getNombreOdontologo());
+
+    }
+
+
 }
